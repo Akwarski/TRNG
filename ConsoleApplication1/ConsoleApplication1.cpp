0c0be5e64a97ede900098a3edf29abc8cdbf524c
Akwarski <akwarski@gmail.com>
2019-03-26 08:35:53 +0100

    better functionality 50%

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,6 +2,7 @@
 //
 
 #include "stdafx.h"
+#include <sstream>
 #include <iostream>
 #include <windows.h>
 #include <cmath>
@@ -10,30 +11,44 @@ using namespace std;
 
 #define M_PI  3.14159265358979323846
 
+void zerowanieTab(int **tabA, int **tabB, int **tabY, int **tabX, int macierzA, int macierzB);
+void dispTab(int **tab, int macierz);
+
 int main()
 {
 	POINT P;
 	int n = 64, k = 5000, wier = 4, kol = 4;
-	int tabA[64][64], tabB[16][16];
-	int wynik_x, wynik_y;
+
+	int macierzA = 64, macierzB = 16;
+
+
+	int **tabA = new int*[64], **tabB = new int*[16], **tabX = new int*[64], **tabY = new int*[16];
+	//int tabX[64][64], tabY[16][16], tabA[64][64], tabB[16][16];
+	int pierwotny_x, pierwotny_y, wynik_x, wynik_y;
 	int pointer = 0, blok = 4;
+	int kolB = 0, wierB = 0;
+	int parzystaPo, parzystaPrzed;
+	double a = GetSystemMetrics(SM_CXSCREEN);
+	double b = GetSystemMetrics(SM_CYSCREEN);
+	double dzielnik_x = a/64, dzielnik_y = b/64;
 
-	for (int i = 0; i < 64; i++) {
-		for (int j = 0; j < 64; j++) {
-			tabA[i][j] = 0;
-			if (i < 16 && j < 16)
-				tabB[i][j] = 0;
-		}
-	}
 
+	tabA[] = new int[macierzA];
+
+	zerowanieTab(tabA,tabB,tabX,tabY, macierzA, macierzB);
+
+	//ilość próbek:
 	while (pointer < 100){
 		if (GetCursorPos(&P)) {
+			pierwotny_x = (P.x/dzielnik_x) + 1;
+			pierwotny_y = (P.y/dzielnik_y) + 1;
+
 			wynik_x = (P.x + P.y) %n;
 			wynik_y = abs(int(P.y + k * sin(n / (2 * M_PI))) % n);
 
-			cout << wynik_x;
-			cout << wynik_y;
+			cout << wynik_x << endl << wynik_y; //wyświetlanie pozycji X i Y myszki
 
+			tabX[pierwotny_x][pierwotny_y] = 1;
 			tabA[wynik_x][wynik_y] = 1;
 
 			pointer++;
@@ -41,31 +56,30 @@ int main()
 			system("cls");
 		}
 	}
+	
+	cout << "Przed entropia: \n";
+	dispTab(tabX, macierzA);
 
-	for (int i = 0; i < 64; i++) {
-		for (int j = 0; j < 64; j++)
-			cout << tabA[i][j];
-		cout << endl;
-	}
+	cout << "\nPo entropii: \n";
+	dispTab(tabA, macierzA);
 
-	int kolB = 0, wierB = 0;
-	int tempK = 0, tempW = 0;
-	int parzysta;
-
-	while (wierB <= 16) {
-		while (kolB <= 16) {
-			parzysta = 0;
-			for (int i = (wier - 4); i < wier; i++) {
-				for (int j = (kol - 4); j < kol; j++) {
+	
+	while (wierB <= 15) {
+		while (kolB <= 15) {
+			parzystaPo = 0;
+			parzystaPrzed = 0;
+			for (int i = (wier - 4); i < wier; ++i) {
+				for (int j = (kol - 4); j < kol; ++j) {
 					if (tabA[i][j] == 1)
-						parzysta++;
+						parzystaPo++;
+					if (tabX[i][j] == 1)
+						parzystaPrzed++;
 				}
 			}
 			kol += 4;
-			tabB[wierB][kolB] = parzysta % 2;
-			//cout << parzysta << " " << tabB[wierB][kolB] << " " << wier << " " << kol << endl;
-			kolB++; //wyzerować przy nowej lini
-			cout << wierB << endl;
+			tabX[wierB][kolB] = parzystaPrzed % 2;
+			tabB[wierB][kolB] = parzystaPo % 2;
+			kolB++;
 		}
 		wier += 4;
 		kol = 0;
@@ -73,49 +87,74 @@ int main()
 		wierB++;
 	}
 
+	cout << "\n\n16x16 przed entropii: \n";
+	for (int i = 0; i < 16; ++i) {
+		for (int j = 0; j < 16; ++j)
+			cout << tabX[i][j] << " ";
+		cout << endl;
+	}
 
-	cout << endl;
-	cout << endl;
-	for (int i = 0; i < 16; i++) {
-		for (int j = 0; j < 16; j++)
+	cout << "\n16x16 po entropia: \n";
+	for (int i = 0; i < 16; ++i) {
+		for (int j = 0; j < 16; ++j)
 			cout << tabB[i][j] << " ";
 		cout << endl;
 	}
 	
-	string wynikk = "";
-	unsigned int liczba_losowa;
-	cout << "Wynik w systemie dwojkowym : " << endl;
-	for (int i = 0; i < 16; i++)
-	{
-		for (int j = 0; j < 16; j++)
-		{
-
+	string wynikPrzed = "";
+	
+	cout << "\nWynik w systemie dwojkowym przed entropia: \n";
+	for (int i = 0; i < 16; ++i){
+		for (int j = 0; j < 16; ++j){
 			ostringstream ss;
-			ss << TabB[i][j];
-			string wynikk = ss.str();
-			cout << wynikk;
-
+			ss << tabB[i][j];
+			wynikPrzed = ss.str();
+			cout << wynikPrzed;
 		}
 	}
 	cout << endl;
 	unsigned long long liczba_los = 0;
 	unsigned long long mnoznik = 1;
-	for (int i = 15; i >= 0; i--)
-	{
-		for (int j = 15; j >= 0; j--)
-		{
-			liczba_los += (TabB[i][j] * mnoznik);
+	for (int i = 15; i >= 0; --i){
+		for (int j = 15; j >= 0; --j){
+			liczba_los += (tabB[i][j] * mnoznik);
 			mnoznik *= 2;
-
 		}
 	}
 	cout << "Liczba wylosowana : ";
 	cout << liczba_los << endl;
 
+	tabA[0][0];
+
+	delete[]tabA;
+
 	system("Pause");
 	return 0;
 }
 
+void zerowanieTab(int **tabA, int **tabB, int **tabY, int **tabX, int macierzA, int macierzB) {
+	for (int i = 0; i < macierzA; ++i) {
+		for (int j = 0; j < macierzA; ++j) {
+			tabA[i][j] = 0;
+			tabY[i][j] = 0;
+			if (i < 16 && j < macierzB) {
+				tabB[i][j] = 0;
+				tabX[i][j] = 0;
+			}
+		}
+	}
+}
+
+void dispTab(int **tab, int macierz) {
+	for (int i = 0; i < macierz; ++i) {
+		for (int j = 0; j < 64; ++j)
+			cout << tab[i][j];
+		cout << endl;
+	}
+}
+
+
+//zliczanie wartości co 8 bitów i następnie dodawanie ich
 
 
 /*
